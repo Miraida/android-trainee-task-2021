d87643cffbe9a8f1e69a2eba8b46d785e62f271c
Miraida <miraidajaanbaeva@gmail.com>
2021-10-29 12:02:52 +0600

    add App.kt, AppModule.kt ,introduce the  hilt into project

---
+++ app/src/main/java/com/geek/android_trainee_task_2021/App.kt
@@ -0,0 +1,8 @@
+package com.geek.android_trainee_task_2021
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App:Application() {
+}
---
+++ app/src/main/java/com/geek/android_trainee_task_2021/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.geek.android_trainee_task_2021.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
