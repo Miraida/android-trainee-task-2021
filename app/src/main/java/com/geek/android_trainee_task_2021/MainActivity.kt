66b8babe5a0c03d21356bb85a4b5ccedab6ee7d9
Miraida <miraidajaanbaeva@gmail.com>
2021-10-29 11:43:30 +0600

    Initial commit

---
+++ app/src/main/java/com/geek/android_trainee_task_2021/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.geek.android_trainee_task_2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
