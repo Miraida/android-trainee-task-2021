beb56afd2a3b72cc716bbb0ada480f33051ff0b4
Miraida <miraidajaanbaeva@gmail.com>
2021-10-29 12:15:26 +0600

    add retrofit,okhttpclient,gson provide functions into AppModule.kt

--- app/src/main/java/com/geek/android_trainee_task_2021/di/AppModule.kt
+++ app/src/main/java/com/geek/android_trainee_task_2021/di/AppModule.kt
@@ -1,11 +1,53 @@
 package com.geek.android_trainee_task_2021.di
 
+import com.geek.android_trainee_task_2021.BuildConfig
+import com.geek.android_trainee_task_2021.data.remote.OpenWeatherApi
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .build()
+    }
 
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient().newBuilder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(retrofit: Retrofit): OpenWeatherApi =
+        retrofit.create(OpenWeatherApi::class.java)
+
+    @Provides
+    fun provideGson(): Gson {
+        return GsonBuilder().setLenient().create()
+    }
 }
